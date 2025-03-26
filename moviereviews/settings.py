ca013df89500bd022354f05c321600f004340a44
almeidalhs <almeidalhs@gmail.com>
2025-03-27 12:39:38 +1300

    modify test env

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -94,14 +94,6 @@ DATABASES = {
     },
 }
 
-if os.getenv('DJANGO_TEST_ENVIRONMENT'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
